ea80cce2a8baab28ca9c1d3cbf18a8e4e6cc7bf9
vfarcic <viktor@farcic.com>
2016-05-13 15:09:48 +0200

    Jenkinsfile

--- jenkins/vars/dockerFlowWorkshop.groovy
+++ jenkins/vars/dockerFlowWorkshop.groovy
@@ -5,7 +5,6 @@ def call(project, flows) {
             "FLOW_PROXY_RECONF_PORT=8081",
             "FLOW_CONSUL_ADDRESS=http://10.100.192.200:8500",
             "FLOW_PROXY_DOCKER_HOST=tcp://10.100.198.200:2375",
-            "REGISTRY=10.100.198.200:5000/",
     ]) {
         def args = "-p " + project + " --flow=" + flows.join(" --flow=")
         sh "docker-flow ${args}"
