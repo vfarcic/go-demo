cfb12a99e0cf6e75b82060c08f22581fe346df5a
Viktor <viktor@farcic.com>
2016-11-22 04:37:17 +0000

    Jenkins global library

--- vars/unit.groovy
+++ vars/unit.groovy
@@ -1,3 +1,11 @@
-def call() {
-    echo "!!!!!!!"
+def call(project) {
+    withEnv([
+            "COMPOSE_FILE=docker-compose-test-local.yml",
+            "COMPOSE_PROJECT_NAME=${project}"
+    ]) {
+
+        stage "Unit"
+        sh "docker-compose run --rm unit"
+        sh "docker build -t go-demo ."
+    }
 }
