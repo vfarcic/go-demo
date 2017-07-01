7beebd7d1227829fa7ef112106b9f59c9db9f734
Viktor <viktor@farcic.com>
2017-07-02 01:40:11 +0200

    Production tess

--- production_test.go
+++ production_test.go
@@ -1,3 +1,5 @@
+// +build production
+
 package main
 
 import (
@@ -22,32 +24,42 @@ func (s *ProductionTestSuite) SetupTest() {
 
 func (s ProductionTestSuite) Test_Hello_ReturnsStatus200() {
 	start := time.Now()
-	max := float64(1)
 	if len(os.Getenv("DURATION")) > 0 {
-		max, _ = strconv.ParseFloat(os.Getenv("DURATION"), 64)
-	}
-	minutes := float64(0)
-	failures := 0
-	errorMsg := ""
-	for time.Since(start).Minutes() < max {
+		max, _ := strconv.ParseFloat(os.Getenv("DURATION"), 64)
+		minutes := float64(0)
+		failures := 0
+		errorMsg := ""
+		for time.Since(start).Minutes() < max {
+			address := fmt.Sprintf("http://%s/demo/hello", s.hostIp)
+			resp, err := http.Get(address)
+
+			if err != nil {
+				failures++
+				errorMsg = err.Error()
+			} else if resp == nil {
+				failures++
+				errorMsg = "Got no response"
+			} else {
+				s.Equal(200, resp.StatusCode)
+			}
+			if time.Since(start).Minutes() > minutes {
+				fmt.Printf("%2.0f out of %2.0f minutes passed\n", minutes, max)
+				minutes++
+			}
+			if failures > 1 {
+				s.Fail(errorMsg)
+			}
+		}
+	} else {
 		address := fmt.Sprintf("http://%s/demo/hello", s.hostIp)
 		resp, err := http.Get(address)
 
 		if err != nil {
-			failures++
-			errorMsg = err.Error()
+			s.Fail(err.Error())
 		} else if resp == nil {
-			failures++
-			errorMsg = "Got no response"
+			s.Fail("Got no response")
 		} else {
 			s.Equal(200, resp.StatusCode)
-		}
-		if time.Since(start).Minutes() > minutes {
-			fmt.Printf("%2.0f out of %2.0f minutes passed\n", minutes, max)
-			minutes++
-		}
-		if failures > 1 {
-			s.Fail(errorMsg)
 		}
 	}
 }
